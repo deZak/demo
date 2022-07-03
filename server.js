937210d32b0feb6c8780a0dd8cefb39c5ddca280
deZak <wpurnadevp@gmail.com>
2022-07-03 12:05:48 +0530

    added as well

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.listen(4444, ()=>console.log('listening on port 44444' ));
 
-app.get('/', (req, res) => res.send('working fine'))
+app.get('/', (req, res) => res.send('working fine as well'))
