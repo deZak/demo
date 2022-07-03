04cefaf5d053dbca738989e9b4a71f197f5acdcf
deZak <wpurnadevp@gmail.com>
2022-07-03 12:05:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.listen(4444, ()=>console.log('listening on port 44444' ));
 
-app.get('/', (req, res) => res.send('working'))
+app.get('/', (req, res) => res.send('working fine'))
